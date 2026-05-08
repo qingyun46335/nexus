f92e836d0e0361d314fe1dab09a87b65360e3718
Xvfzy <3241729816zlj@gmail.com>
2026-05-08 23:06:51 +0800

    fix(result): 针对 result 添加msg，用于存储成功或是失败的提示信息

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,14 +1,16 @@
 import { BaseError } from "../error/error";
 
-export type Result<T> = { v: T; e: null } | { v: null; e: BaseError };
+export type Result<T> = { v: T; e: null, msg: string } | { v: null; e: BaseError, msg: string };
 
-export const Ok = <T>(value: T): Result<T> => ({ v: value, e: null });
+export const Ok = <T>(value: T): Result<T> => ({ v: value, e: null, msg: "" });
 
-export const Err = <T>(err: BaseError): Result<T> => ({ v: null, e: err });
+export const OkMsg = <T>(msg: string, value: T): Result<T> => ({ v: value, e: null, msg: msg });
+
+export const Err = <T>(err: BaseError): Result<T> => ({ v: null, e: err, msg: err.message });
 
 // 便捷工厂：wrap 一个底层 Error 为自定义类型
 export const ErrFrom = <T>(
     cls: new (msg: string, cause?: Error) => BaseError,
     message: string,
     cause?: Error
-): Result<T> => ({ v: null, e: new cls(message, cause) });
+): Result<T> => ({ v: null, e: new cls(message, cause), msg: message });
