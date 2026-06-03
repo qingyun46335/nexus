1d784c1717d76d94bd91d48c42e59bdf9f126b8b
Xvfzy <3241729816zlj@gmail.com>
2026-06-03 10:00:30 +0800

    feat: 添加数据库表结构

---
+++ packages/server/src/db/schema.ts
@@ -0,0 +1,40 @@
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+import { previewTypeEnum } from "../utils/preview_type_mapping";
+
+export const article = sqliteTable("article", {
+  id: text("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  status: text("status").$type<"published" | "draft" | "archived">().notNull(),
+  createdAt: text("created_at").notNull(),
+  updatedAt: text("updated_at").notNull(),
+  views: integer("views").notNull(),
+  wordCount: integer("word_count").notNull(),
+  tags: text("tags").notNull(),
+});
+
+export const articleFile = sqliteTable("article_file", {
+  id: text("id").primaryKey(),
+  uuid: text("uuid").notNull(),
+  suffix: text("suffix").notNull(),
+  name: text("name").notNull(),
+  previewType: text("preview_type").$type<previewTypeEnum>().notNull(),
+  size: integer("size").notNull(),
+  filePath: text("file_path").notNull(),
+  relativePath: text("relative_path").notNull(),
+  articleId: text("article_id").notNull(),
+})
+
+export const tag = sqliteTable("tag", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  count: integer("count").notNull(),  //  关联文章数
+  views: integer("views").notNull(), // 标签总浏览数
+  status: text("status").$type<"active" | "inactive">().notNull(),
+})
+
+export const articleToTag = sqliteTable("article_to_tag", {
+  id: text("id").primaryKey(),
+  articleId: text("article_id").notNull(),
+  tagId: text("tag_id").notNull(),
+})
