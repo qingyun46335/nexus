1b76eed63b054e1251921b51fa04e27ff5ed79c2
Xvfzy <3241729816zlj@gmail.com>
2026-06-03 16:51:12 +0800

    chore: 更新文件包路径

--- packages/client/src/views/upload-page.ts
+++ packages/client/src/views/upload-page.ts
@@ -2,7 +2,7 @@ import { customElement } from "lit/decorators.js";
 import { DaisyUIElement } from "../components/daisy-ui-element";
 import { html } from "lit";
 
-import "../components/blog-uploader";
+import "../components/admin/blog-uploader";
 
 @customElement("upload-page")
 export class UploadPage extends DaisyUIElement {
