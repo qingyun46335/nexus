5733847c84a402f678f8146016c3f55220c7733c
Xvfzy <3241729816zlj@gmail.com>
2026-06-10 22:17:04 +0800

    feat: 导入前台路由并完成注册

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,16 +1,19 @@
 import { AdminRoute } from "./route/admin/admin_route";
 import { ApiRoute } from "./route/api_route";
 import { AdminArticleRoute } from "./route/admin/article_route";
-import { AssetsRoute } from "./route/admin/assets_route";
+import { AdminAssetsRoute } from "./route/admin/assets_route";
 import { RootRoute } from "./route/root_route";
 import { AdminTagRoute } from "./route/admin/tag_route";
-import { TestRoute } from "./route/test_route";
 import Server from "./server/server";
 import {
   ApiDocCollector,
   ApiDocCollectorVoidImpl,
 } from "./utils/api_doc_collector";
 import { AdminStatsRoute } from "./route/admin/stats_route";
+import { ClientRoute } from "./route/client/client_route";
+import { ArticleRoute } from "./route/client/article_route";
+import { AssetsRoute } from "./route/client/assets_route";
+import { TagRoute } from "./route/client/tag_toute";
 
 export function createApp(apiDocCollector?: ApiDocCollector) {
   const route = createRoute(apiDocCollector);
@@ -34,12 +37,16 @@ export function createRoute(apiDocCollector?: ApiDocCollector): RootRoute {
 
   const api = route.setRoute(new ApiRoute(apiDocCollector));
 
-  api.setRoute(new TestRoute(apiDocCollector));
   const admin = api.setRoute(new AdminRoute(apiDocCollector));
   admin.setRoute(new AdminArticleRoute(apiDocCollector));
-  admin.setRoute(new AssetsRoute());
+  admin.setRoute(new AdminAssetsRoute());
   admin.setRoute(new AdminTagRoute(apiDocCollector))
   admin.setRoute(new AdminStatsRoute(apiDocCollector))
+
+  const client = api.setRoute(new ClientRoute(apiDocCollector))
+  client.setRoute(new ArticleRoute(apiDocCollector))
+  client.setRoute(new AssetsRoute(apiDocCollector))
+  client.setRoute(new TagRoute(apiDocCollector))
 
   return route;
 }
