b60e3003c5be0d0075ba88201dd8820700af6e37
Xvfzy <3241729816zlj@gmail.com>
2026-06-10 21:45:19 +0800

    feat: 添加客户端静态资源访问地址

--- packages/server/src/config/base_config.ts
+++ packages/server/src/config/base_config.ts
@@ -1,5 +1,6 @@
 export const AssetsConfig = {
   NEXUS_FILE_BUCKET: "/api/admin/assets/file",
+  CLIENT_FILE_PATH_PREFIX: "/api/client/assets", //  完整路径：/api/client/assets/ + 文件大类 + / + uuid名称
 };
 
 export const BaseConfig = {
