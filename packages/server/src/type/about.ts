7def6d98d46e4b81296592de77b547aa09d335a8
Xvfzy <3241729816zlj@gmail.com>
2026-06-18 16:21:57 +0800

    feat: 添加用于响应的视图数据

--- packages/server/src/type/about.ts
+++ packages/server/src/type/about.ts
@@ -1,5 +1,15 @@
 export interface FriendLink {
-    id: number;
+    id: string;
+    name: string;        // 站名
+    url: string;         // 链接
+    avatar: string;      // 头像/favicon URL
+    description: string; // 一句话介绍
+    author: string;      // 作者名
+    status: 1 | 0;
+}
+
+export interface FriendLinkVo {
+    id: string;
     name: string;        // 站名
     url: string;         // 链接
     avatar: string;      // 头像/favicon URL
