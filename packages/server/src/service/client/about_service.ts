9aa805830006ae1c0f9907f5af49a1bdacca11fe
Xvfzy <3241729816zlj@gmail.com>
2026-06-18 16:29:10 +0800

    refactor: 重构友链获取

--- packages/server/src/service/client/about_service.ts
+++ packages/server/src/service/client/about_service.ts
@@ -1,12 +1,20 @@
 import { DrizzleD1Database } from "drizzle-orm/d1";
-import { FriendLink } from "../../type/about";
+import { FriendLinkVo } from "../../type/about";
 import { ErrFrom, Ok, Result, to } from "../../utils/result";
 import * as schema from "../../db/schema";
 import { DBError } from "../../error/error";
+import { eq } from "drizzle-orm";
 
 export class AboutService {
-    public async getFriends(db: DrizzleD1Database<typeof schema>,): Promise<Result<FriendLink[]>> {
-        const res = await to(db.query.friendLink.findMany())
+    public async getFriends(db: DrizzleD1Database<typeof schema>,): Promise<Result<FriendLinkVo[]>> {
+        const res = await to(db.select({
+            id: schema.friendLink.id,
+            name: schema.friendLink.name,         // 站名
+            url: schema.friendLink.avatar,         // 链接
+            avatar: schema.friendLink.avatar,      // 头像/favicon URL
+            description: schema.friendLink.description, // 一句话介绍
+            author: schema.friendLink.author,      // 作者名
+        }).from(schema.friendLink).where(eq(schema.friendLink.status, 1)))
 
         if (res.e) {
             return ErrFrom(DBError, "数据库异常", res.e)
